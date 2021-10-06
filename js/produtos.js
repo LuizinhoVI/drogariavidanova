5443421ad1a6baf759e24e317adb2813da5a1450
luizinhovi <beat.luiz23@hotmail.com>
2021-10-06 14:08:07 -0300

    att

---
+++ js/produtos.js
@@ -0,0 +1,71 @@
+function suplementos() {
+    var abrir = document.getElementById('suplementos').style;
+    if (abrir.display == "none") {
+        abrir.display = "block";
+    } else { abrir.display = "none"; }
+
+}
+
+function massa() {
+    var abrir = document.getElementById('massa').style;
+    if (abrir.display == "none") {
+        abrir.display = "block";
+    } else { abrir.display = "none"; }
+
+}
+
+function maquiagem() {
+    var abrir = document.getElementById('maquiagem').style;
+    if (abrir.display == "none") {
+        abrir.display = "block";
+    } else { abrir.display = "none"; }
+
+}
+
+function rosto() {
+    var abrir = document.getElementById('rosto').style;
+    if (abrir.display == "none") {
+        abrir.display = "block";
+    } else { abrir.display = "none"; }
+
+}
+
+function volt() {
+    var abrir = document.getElementById('volt').style;
+    if (abrir.display == "none") {
+        abrir.display = "block";
+    } else { abrir.display = "none"; }
+
+}
+
+function cosmeticos() {
+    var abrir = document.getElementById('cosmeticos').style;
+    if (abrir.display == "none") {
+        abrir.display = "block";
+    } else { abrir.display = "none"; }
+
+}
+
+function medicamentos() {
+    var abrir = document.getElementById('medicamentos').style;
+    if (abrir.display == "none") {
+        abrir.display = "block";
+    } else { abrir.display = "none"; }
+
+}
+
+function olho() {
+    var abrir = document.getElementById('olho').style;
+    if (abrir.display == "none") {
+        abrir.display = "block";
+    } else { abrir.display = "none"; }
+
+}
+
+function labio() {
+    var abrir = document.getElementById('labio').style;
+    if (abrir.display == "none") {
+        abrir.display = "block";
+    } else { abrir.display = "none"; }
+
+}
--- js/script.js
+++ js/script.js
@@ -1,38 +1,22 @@
-     // Get the Sidebar
-     var mySidebar = document.getElementById("mySidebar");
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
 
-     // Get the DIV with overlay effect
-     var overlayBg = document.getElementById("myOverlay");
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
 
-     // Toggle between showing and hiding the sidebar, and add overlay effect
-     function w3_open() {
-         if (mySidebar.style.display === 'block') {
-             mySidebar.style.display = 'none';
-             overlayBg.style.display = "none";
-         } else {
-             mySidebar.style.display = 'block';
-             overlayBg.style.display = "block";
-         }
-     }
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+        overlayBg.style.display = "none";
+    } else {
+        mySidebar.style.display = 'block';
+        overlayBg.style.display = "block";
+    }
+}
 
-     // Close the sidebar with the close button
-     function w3_close() {
-         mySidebar.style.display = "none";
-         overlayBg.style.display = "none";
-     }
-     numero = 0;
-
-     function abrir() {
-
-         var abrir = document.getElementById('open').style;
-
-         if (numero >= 0) {
-             abrir.display = "none";
-
-         } else {
-             abrir.display = "block";
-
-         }
-
-
-     }
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+    overlayBg.style.display = "none";
+}

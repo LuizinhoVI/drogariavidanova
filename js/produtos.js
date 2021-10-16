ea2c6e1abaadcb013fbf7cd41cebff2b84483bdd
luizinhovi <beat.luiz23@hotmail.com>
2021-10-16 11:05:14 -0300

    att

--- js/produtos.js
+++ js/produtos.js
@@ -1,103 +1,117 @@
-function suplementos() {
-    var abrir = document.getElementById('massa').style;
-    var abrir1 = document.getElementById('suplemento').style;
-    var temas = document.getElementById('temas');
-    if (abrir.display == "none") {
-        abrir.display = "block";
-        abrir1.display = "block";
-        temas.style.display = "block";
-        temas.innerHTML = "Suplementos";
+ function suplementos() {
+     var abrir = document.getElementById('massa').style;
+     var abrir1 = document.getElementById('suplemento').style;
+     var paginas = document.getElementById('paginas').style;
+     var temas = document.getElementById('temas');
+     if (abrir.display == "none") {
+         abrir.display = "block";
+         abrir1.display = "block";
+         paginas.display = "block"
+         temas.style.display = "block";
+         temas.innerHTML = "Suplementos";
 
-    } else {
-        abrir.display = "none";
-        abrir1.display = "none";
-        temas.style.display = "none";
+     } else {
+         abrir.display = "none";
+         abrir1.display = "none";
+         paginas.display = "none";
+         temas.style.display = "none";
 
-    }
+     }
 
-}
+ }
+
+ function voxx() {
+     var abrir = document.getElementById('voxx').style;
+     if (abrir.display == "none") {
+         abrir.display = "block";
+     } else { abrir.display = "none"; }
+ }
 
 
-function maquiagem() {
-    var abrir = document.getElementById('cosmetico').style;
-    if (abrir.display == "none") {
-        abrir.display = "block";
-    } else { abrir.display = "none"; }
+ function maquiagem() {
+     var abrir = document.getElementById('cosmetico').style;
+     if (abrir.display == "none") {
+         abrir.display = "block";
+     } else { abrir.display = "none"; }
 
-}
+ }
 
-function rosto() {
-    var abrir = document.getElementById('rosto').style;
-    if (abrir.display == "none") {
-        abrir.display = "block";
-    } else { abrir.display = "none"; }
+ function rosto() {
+     var abrir = document.getElementById('rosto').style;
+     if (abrir.display == "none") {
+         abrir.display = "block";
+     } else { abrir.display = "none"; }
 
-}
+ }
 
-function volt() {
-    var abrir = document.getElementById('volt').style;
-    if (abrir.display == "none") {
-        abrir.display = "block";
-    } else { abrir.display = "none"; }
+ function volt() {
+     var abrir = document.getElementById('volt').style;
+     if (abrir.display == "none") {
+         abrir.display = "block";
+     } else { abrir.display = "none"; }
 
-}
+ }
 
-function cosmeticos() {
-    var abrir = document.getElementById('cosmeticos').style;
-    var abrir1 = document.getElementById('cosmetico').style;
+ function cosmeticos() {
+     var open = document.getElementById('suplemento').style;
+     var abrir = document.getElementById('cosmeticos').style;
+     var abrir1 = document.getElementById('cosmetico').style;
+     var paginas = document.getElementById('paginas').style;
 
-    var temas = document.getElementById('temas');
+     var temas = document.getElementById('temas');
 
-    if (abrir.display == "none") {
-        abrir.display = "block";
-        abrir1.display = "block";
-        temas.style.display = "block";
-        temas.innerHTML = "Cosmeticos";
+     if (abrir.display == "none") {
+         abrir.display = "block";
+         abrir1.display = "block";
+         paginas.display = "block";
+         temas.style.display = "block";
+         temas.innerHTML = "Cosmeticos";
 
-    } else {
-        abrir.display = "none";
-        abrir1.display = "none";
-        temas.style.display = "none";
-    }
+     } else {
+         abrir.display = "none";
+         abrir1.display = "none";
+         paginas.display = "none";
+         temas.style.display = "none";
+     }
 
-}
+ }
 
-function cabelo() {
-    var abrir = document.getElementById('cabelo').style;
-    if (abrir.display == "none") {
-        abrir.display = "block";
-    } else { abrir.display = "none"; }
+ function cabelo() {
+     var abrir = document.getElementById('cabelo').style;
+     if (abrir.display == "none") {
+         abrir.display = "block";
+     } else { abrir.display = "none"; }
 
-}
+ }
 
-function maquiagem() {
-    var abrir = document.getElementById('pro-maquiagem').style;
-    if (abrir.display == "none") {
-        abrir.display = "block";
-    } else { abrir.display = "none"; }
+ function maquiagem() {
+     var abrir = document.getElementById('pro-maquiagem').style;
+     if (abrir.display == "none") {
+         abrir.display = "block";
+     } else { abrir.display = "none"; }
 
-}
+ }
 
-function medicamentos() {
-    var abrir = document.getElementById('medicamentos').style;
-    if (abrir.display == "none") {
-        abrir.display = "block";
-    } else { abrir.display = "none"; }
+ function medicamentos() {
+     var abrir = document.getElementById('medicamentos').style;
+     if (abrir.display == "none") {
+         abrir.display = "block";
+     } else { abrir.display = "none"; }
 
-}
+ }
 
-function olho() {
-    var abrir = document.getElementById('olho').style;
-    if (abrir.display == "none") {
-        abrir.display = "block";
-    } else { abrir.display = "none"; }
+ function olho() {
+     var abrir = document.getElementById('olho').style;
+     if (abrir.display == "none") {
+         abrir.display = "block";
+     } else { abrir.display = "none"; }
 
-}
+ }
 
-function labio() {
-    var abrir = document.getElementById('labio').style;
-    if (abrir.display == "none") {
-        abrir.display = "block";
-    } else { abrir.display = "none"; }
+ function labio() {
+     var abrir = document.getElementById('labio').style;
+     if (abrir.display == "none") {
+         abrir.display = "block";
+     } else { abrir.display = "none"; }
 
-}
+ }

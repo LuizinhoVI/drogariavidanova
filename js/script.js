1bcf50c78edf87cba025e0c90b50d94892fc1141
luizinhovi <beat.luiz23@hotmail.com>
2021-11-04 14:17:43 -0300

    att

--- js/script.js
+++ js/script.js
@@ -20,3 +20,20 @@ function w3_close() {
     mySidebar.style.display = "none";
     overlayBg.style.display = "none";
 }
+
+function pesquisa() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("entrada");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("all-remedio");
+    li = ul.getElementsByTagName("div");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("p")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}

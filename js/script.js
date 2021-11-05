45b0ea2144e5601228da0afe812b8bd6c0003119
luizinhovi <beat.luiz23@hotmail.com>
2021-11-05 14:24:53 -0300

    att

--- js/script.js
+++ js/script.js
@@ -40,3 +40,22 @@ function pesquisar() {
     }
 
 }
+
+function pesquisar_cosmetico() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("entrada");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("cosmeticos");
+    li = ul.getElementsByTagName("div");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("p")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+            console.log(li[i])
+        }
+    }
+
+}
